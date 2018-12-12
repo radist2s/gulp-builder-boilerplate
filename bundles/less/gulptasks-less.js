6a7764c7599200f4246594ea1a2d09be7c66124a
Alex Batalov <radist2s@gmail.com>
2018-12-12 18:52:29 +0500

    Builder tasks updated

--- bundles/less/gulptasks-less.js
+++ bundles/less/gulptasks-less.js
@@ -3,6 +3,7 @@ var gutil = require('gulp-util')
 var notifier = require('node-notifier')
 var path = require('path')
 var liveReloadModule
+var htmlInjector
 
 var args = require('args-parser')(process.argv)
 
@@ -13,6 +14,51 @@ var globsResolvePath = require('../lib').globsResolvePath
 var bundleCaption = require('../lib').getBundleCaption(__dirname)
 
 var cssOutDir = lessConfig.cssOutDir
+
+var browserSync;
+
+function browserSyncInit() {
+    if (!browserSync) {
+        browserSync = require('browser-sync').create()
+    }
+
+    if (browserSync.instance.active) {
+        return
+    }
+
+    htmlInjector = require('bs-html-injector')
+
+    browserSync.init({
+        proxy: {
+            target: 'qatar.ngrok.taggart.ru',
+
+            reqHeaders: function () {
+                const headers = {'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1'}
+
+                return {}
+            },
+
+            proxyReq: [
+                function(proxyReq) {
+                    const headers = proxyReq.getHeaders()
+
+                    if (headers['user-agent']) {
+                        proxyReq.setHeader('user-agent', headers['user-agent']);
+                    }
+                }
+            ]
+        },
+        open: false,
+        snippetOptions: {
+            whitelist: ['/*/ajax/', '/*/*/ajax/']
+        },
+        cacheBusting: "[name]_[hash:8]"
+    })
+
+    browserSync.use(htmlInjector)
+
+    gulp.watch('../../parts/**/*.php').on('change', htmlInjector);
+}
 
 function liveReload() {
     if (!liveReloadModule) {
@@ -25,6 +71,14 @@ function liveReload() {
 
 gulp.task('less', 'Build LESS files with PostCSS pipes and exit ' + bundleCaption, function () {
     return createLessStream()
+})
+
+gulp.task('less-browsersync', 'Starts less task and BrowserSync serving ' + bundleCaption, function () {
+    browserSyncInit()
+
+    return createLessStream()
+        .pipe(liveReload()())
+        .pipe(browserSync.stream())
 })
 
 gulp.task('less-livereload', function () {
@@ -40,7 +94,7 @@ gulp.task('less-livereload', function () {
 
 const COMPILE_FIRST_ARG = 'compile-first'
 
-gulp.task('less-watch', 'Run LESS watch with LiveReload ' + bundleCaption,
+gulp.task('less-watch', 'Run LESS watch with BrowserSync ' + bundleCaption,
     args[COMPILE_FIRST_ARG] ? ['less'] : [],
     function () {
         gulp.watch(lessConfig.watchGlobs, function (event) {
@@ -59,7 +113,7 @@ gulp.task('less-watch', 'Run LESS watch with LiveReload ' + bundleCaption,
             catch (e) {
             }
 
-            gulp.start('less-livereload')
+            gulp.start('less-browsersync')
         })
     },
     {
@@ -82,7 +136,8 @@ function createLessStream() {
         postcssMath = require('postcss-math'),
         sourcemaps = require('gulp-sourcemaps'),
         autoprefixer = require('autoprefixer'),
-        gulpCleanCSS = require('gulp-clean-css')
+        gulpCleanCSS = require('gulp-clean-css'),
+        NpmImportPlugin = require('less-plugin-npm-import')
 
     var postCssPipe = postcss([
         postcssMath,
@@ -91,13 +146,19 @@ function createLessStream() {
 
     var globs = globsResolvePath(lessConfig.compileGlobs, process.cwd())
 
-    return gulp
+    var lessStream = gulp
         .src(globs)
 
         .pipe(sourcemaps.init())
 
         // Less compilation
-        .pipe(less({ieCompat: true, paths: [cssOutDir]}))
+        .pipe(less({
+            ieCompat: true,
+            paths: [cssOutDir],
+            plugins: [
+                new NpmImportPlugin({prefix: '~'})
+            ]
+        }))
         .on('error', function (error) {
             try {
                 notifier.notify({title: 'Less compilation error', message: error.message})
@@ -130,4 +191,6 @@ function createLessStream() {
 
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(cssOutDir))
+
+    return lessStream
 }
